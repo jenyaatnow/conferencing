8a22ecba447949048e74a65c76f24377775da900
Evgeniy Surin <jenyaatnow@gmail.com>
2022-12-06 12:29:53 +0400

    Second WS sample

--- back/conf-engine/src/main/scala/WsSample.scala
+++ back/conf-engine/src/main/scala/WsSample.scala
@@ -3,11 +3,11 @@ package com.bravewave.conferencing.conf
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import akka.http.scaladsl.model.ws.{BinaryMessage, Message, TextMessage}
+import akka.http.scaladsl.model.ws.{BinaryMessage, TextMessage}
 import akka.http.scaladsl.server.Directives._
-import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.scaladsl.{Sink, Source}
 
+import scala.concurrent.duration.DurationInt
 import scala.io.StdIn
 
 
@@ -15,28 +15,26 @@ object WsSample extends App {
   private implicit val system = ActorSystem(Behaviors.empty, "my-system")
   private implicit val executionContext = system.executionContext
 
-  private def greeter: Flow[Message, Message, Any] =
-    Flow[Message].mapConcat {
-      case tm: TextMessage =>
-        TextMessage(Source.single("Hello ") ++ tm.textStream ++ Source.single("!")) :: Nil
-      case bm: BinaryMessage =>
-        // ignore binary messages but drain content to avoid the stream being clogged
-        bm.dataStream.runWith(Sink.ignore)
-        Nil
-    }
+  val testSource = Source
+    .repeat("Hello")
+    .throttle(1, 1.seconds)
+    .map(TextMessage(_))
+    .limit(1000)
 
-  private val route =
-    pathPrefix("sample") {
-      path("hello") {
-        get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
-        }
-      } ~ path("greeter") {
-        handleWebSocketMessages(greeter)
+  val router =
+    path("ws") {
+      extractWebSocketUpgrade { upgrade =>
+        complete(upgrade.handleMessagesWithSinkSource(
+          Sink.foreach {
+            case tm: TextMessage => tm.textStream.runForeach(print)
+            case bm: BinaryMessage => bm.dataStream.runWith(Sink.ignore)
+          },
+          testSource,
+        ))
       }
     }
 
-  private val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+  private val bindingFuture = Http().newServerAt("localhost", 8080).bind(router)
 
   println(s"Server now online. Please navigate to http://localhost:8080/sample/hello\nPress RETURN to stop...")
   StdIn.readLine() // let it run until user presses return
